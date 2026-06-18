bb4c54598c6a734fc0111ed27e2abd2dd59ae9a8
[Aya] <[tanukichiaya@gmail.com]>
2026-06-18 11:35:24 +0900

    正誤機能の追加

--- git_homework.py
+++ git_homework.py
@@ -6,4 +6,7 @@ print("これから数字当てゲームを作成します。")
 answer = random.randint(100, 999)
 guess = input("3桁の数字を入力してください：")
 
-print("あなたの回答は", guess, "です。")
+if guess == answer:
+    print("正解です！")
+else:
+    print("不正解です。")
