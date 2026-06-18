20e2d67bfeb6de8b0cab5e672dff7d6fde277b76
[Aya] <[tanukichiaya@gmail.com]>
2026-06-18 12:38:10 +0900

    複数回答の可能化

--- git_homework.py
+++ git_homework.py
@@ -6,7 +6,13 @@ print("これから数字当てゲームを作成します。")
 answer = random.randint(100, 999)
 guess = input("3桁の数字を入力してください：")
 
-if guess == answer:
-    print("正解です！")
-else:
-    print("不正解です。")
+while True:
+    guess = int(input("3桁の数字を入力してください："))
+
+    print("あなたの回答は", guess, "です。")
+
+    if guess == answer:
+        print("正解です！")
+        break
+    else:
+        print("不正解です。もう一度入力してください。")
